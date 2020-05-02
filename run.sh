cdda4c3523b064f928a48868b38a8fc6e46daf7f
agrilive <56161787+agrilive@users.noreply.github.com>
2020-05-02 10:23:23 +0800

    upload files

---
+++ run.sh
@@ -0,0 +1,9 @@
+# Install libraries in requirements.txt
+sudo apt-get install python-matplotlib
+pip install -r requirements.txt
+
+# Execute main script
+pip3 install -r requirements.txt
+
+# Execute python scripts
+python3 mlp/main.py
